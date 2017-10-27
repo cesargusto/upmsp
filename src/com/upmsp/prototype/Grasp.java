e95cae46e9a38ccd60bd56f9c1966bef7c5b14b6
cesar <cesar@debntic>
2017-10-27 19:43:01 -0200

    Protótipo Grasp

--- src/com/upmsp/prototype/Grasp.java
+++ src/com/upmsp/prototype/Grasp.java
@@ -1,36 +1,41 @@
 package com.upmsp.prototype;
 
-import com.upmsp.metaheuristic.ils.LocalSearchILS;
-import com.upmsp.structure.Solution;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 public class Grasp {
 	
-	private LocalSearchILS ls_ils;
-	
-	public Grasp() {
-		LocalSearchILS ls_ils = new LocalSearchILS();
-	}
-	
-	public Solution execute_grasp(Solution s, int grasp_max) throws CloneNotSupportedException {
-		
-		Solution s_melhor;
-		int fo_s_melhor = Integer.MAX_VALUE;
-		
-		while(grasp_max > 0) {
-		
-			s = this.construction_grasp(s);
-			s = this.ls_ils.local_search_ils(s);
-			int fo_s_aux = s.makespan();
-			if(fo_s_aux < fo_s_melhor) {
-				fo_s_melhor = fo_s_aux;
-				s_melhor = s.clone();
-			}
-		}
-		return s;
-	}
-	
-	public Solution construction_grasp(Solution s) {
-		return s;
-	}
+	public ArrayList<Integer> execute_grasp(ArrayList<Integer> sol_init, double alfa){
 
+		ArrayList<Integer> LC = new ArrayList<>(sol_init);
+		ArrayList<Integer> LCR = new ArrayList<>();
+		ArrayList<Integer> solution = new ArrayList<>();
+		Random rnd = new Random();
+
+		int g_min = -1;
+		int g_max = -1;
+		
+		for(int j = 0; j < LC.size();j++){
+			Collections.sort(LC);
+
+			g_min = LC.get(0);
+			g_max = LC.get(LC.size()-1);
+			
+			double valor = Double.MAX_VALUE;
+			
+			valor = g_min + alfa*(g_max - g_min);
+			
+			for(int i = 0;i < LC.size();i++){
+				int get = LC.get(i); 
+				if( get < valor){
+					LCR.add(LC.get(i));
+					LC.remove(i);
+				}
+			}
+			int index = rnd.nextInt(LCR.size());
+			solution.add(LCR.get(index));
+		}
+		return solution;
+	}
 }
--- src/com/upmsp/prototype/Teste.java
+++ src/com/upmsp/prototype/Teste.java
@@ -1,34 +1,50 @@
 package com.upmsp.prototype;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
 public class Teste {
     
     public static void main(String[] args) {
-        Maquina maq1 = new Maquina();
-        maq1.addJobToMaq(1);
-        maq1.addJobToMaq(6);
-        maq1.addJobToMaq(8);
-        maq1.addJobToMaq(3);
         
-        Maquina maq2 = new Maquina();
-        maq2.addJobToMaq(7);
-        maq2.addJobToMaq(0);
+    	//Maquina maq1 = new Maquina();
+        //maq1.addJobToMaq(1);
+        //maq1.addJobToMaq(6);
+        //maq1.addJobToMaq(8);
+        //maq1.addJobToMaq(3);
         
-        Maquina maq3 = new Maquina();
-        maq3.addJobToMaq(4);
-        maq3.addJobToMaq(2);
-        maq3.addJobToMaq(5);
+        //Maquina maq2 = new Maquina();
+        //maq2.addJobToMaq(7);
+        //maq2.addJobToMaq(0);
         
-        Solucao solucao = new Solucao();
-        solucao.setMaqToSol(maq1);
-        solucao.setMaqToSol(maq2);
-        solucao.setMaqToSol(maq3);
+        //Maquina maq3 = new Maquina();
+        //maq3.addJobToMaq(4);
+        //maq3.addJobToMaq(2);
+        //maq3.addJobToMaq(5);
         
-        solucao.print_solucao();
+        //Solucao solucao = new Solucao();
+        //solucao.setMaqToSol(maq1);
+        //solucao.setMaqToSol(maq2);
+        //solucao.setMaqToSol(maq3);
         
-        System.out.println("-------------------------------");
+        //solucao.print_solucao();
         
-        BuscaLocal bl = new BuscaLocal(solucao);
-        bl.change_Maq();
+        //System.out.println("-------------------------------");
         
+        //BuscaLocal bl = new BuscaLocal(solucao);
+        //bl.change_Maq();
+    	
+		Random rnd = new Random();
+		ArrayList<Integer> lista = new ArrayList<>();
+		
+		for(int i = 0;i < 15; i++){
+			lista.add(rnd.nextInt(50));
+		}
+		
+		Collections.sort(lista);
+		System.out.println(lista);
+		Grasp grasp = new Grasp();
+		System.out.println(grasp.execute_grasp(lista, 0.6));
     }
 }
